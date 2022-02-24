65405f13a95a134457913cb7497261889fe76e77
danielornowski <danielornowski00@gmail.com>
2022-02-24 21:33:00 +0100

    a

--- apiProject/apiProject/settings.py
+++ apiProject/apiProject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(=*x@!4-6yu-+f3wo01hmut1@gha1u_nr6_0meped23l2nu-h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'projects',
-    'apiProject',
 ]
 
 MIDDLEWARE = [
